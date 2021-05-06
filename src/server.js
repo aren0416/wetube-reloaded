d71f2f15dd2d48723c889d856931ddefd9aeeb36
aren0416 <aren0416@gmail.com>
2021-05-06 16:47:58 +0900

    request

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("홈홈홈홈");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 👀`);
 
