826c3050d92718fdd7fcc90ff5040dcee7b03daa
aren0416 <aren0416@gmail.com>
2021-05-08 21:45:56 +0900

    middleware end

--- src/server.js
+++ src/server.js
@@ -8,27 +8,12 @@ const logger = (req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 };
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue");
-  next();
-};
 
 const handleHome = (req, res) => {
   return res.send("ggg");
 };
 
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge");
-};
-
-app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.get("/", logger, handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 👀 ⭐️`);
