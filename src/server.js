e435c3ddf7f6d49f7d4c6528bfae1c6e51ef64a6
aren0416 <aren0416@gmail.com>
2021-05-08 21:41:30 +0900

    middleware example

--- src/server.js
+++ src/server.js
@@ -4,18 +4,33 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to : ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue");
   next();
 };
 
 const handleHome = (req, res) => {
-  return res.end();
+  return res.send("ggg");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
-  console.log(`✅ Server listening on port http://localhost:${PORT} 👀⭐️`);
+  console.log(`✅ Server listening on port http://localhost:${PORT} 👀 ⭐️`);
 
 app.listen(PORT, handleListening);
